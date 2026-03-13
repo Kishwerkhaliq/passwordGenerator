26179deb895c08ff111b3b258fe48742db880a88
Kishwerkhaliq <khaliqkishwer@gmail.com>
2026-03-13 11:26:43 +0000

    code has been added

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,1 +1,27 @@
 
+import secrets
+import string
+
+# Ask the user for the password length
+len = int(input("Enter the desired password length: "))
+
+
+def generate_password(length=(len-4), use_upper=True, use_lower=True, use_digits=True, use_symbols=True):
+    character_pool = ""
+
+    if use_upper:
+        character_pool += string.ascii_uppercase
+    if use_lower:
+        character_pool += string.ascii_lowercase
+    if use_digits:
+        character_pool += string.digits
+    if use_symbols:
+        character_pool += "!@#$%^&*()-_=+[]{};:,.<>?"
+
+    if not character_pool:
+        return "Error: No character types selected!"
+
+    password = "".join(secrets.choice(character_pool) for _ in range(len))
+    return password
+
+print("Generated Password:", generate_password(len))
