d8a553b69583b5bd5cdebdca3f40e5463cc04da3
Faithx754 <faithagun@icloud.com>
2026-03-13 11:49:53 +0000

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,1 +1,7 @@
+Ask user what characters to use 
 
+use_upper = input("Include uppercase letters? (y/n: ").lower() == "y"
+use_upper = input("Include lowercase letters? (y/n: ").lower() == "y"
+
+use_digits = input("Include numbers? (y/n): ").lower() == "y"
+use_symbols = input("Include symbols? (y/n: ").lower() == "y"
